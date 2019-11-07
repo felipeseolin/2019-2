8f842ba5a3b57280f05826f131123121b9ebc095
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-06 21:02:19 -0300

    Correções

--- week-13/app/src/main/java/com/example/app/apresentacao/ClienteController.java
+++ week-13/app/src/main/java/com/example/app/apresentacao/ClienteController.java
@@ -8,11 +8,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Set;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ClienteController {
 
